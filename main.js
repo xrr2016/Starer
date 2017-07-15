3a56d17b8c83ca80623691efb212c078944ae320
xrr2016 <xiaoranran1993@163.com>
2017-07-15 18:55:47 +0800

    :zap;

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   win = new BrowserWindow({ width: 800, height: 600})
-  win.loadUrl(url.format({
+  win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file',
     slashes: true
