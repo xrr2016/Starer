a3ad76c27b400c8a2c4cd5d3d649e67d5949a9ce
xrr2016 <xiaoranran1993@163.com>
2017-10-18 01:56:20 +0800

    :boom:

--- asar.js
+++
@@ -1,9 +0,0 @@
-const asar = require('asar')
-const { resolve } = require('path')
-
-const src = resolve(__dirname, 'src')
-const name = 'starer.asar'
-
-asar.createPackage(src, name, () => {
-  console.log('Asar done.')
-})
