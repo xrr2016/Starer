954adc2fa03b9139ee9cf736e1d06694672ae93d
xrr2016 <xiaoranran1993@163.com>
2017-10-29 00:15:23 +0800

    :fire: working

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 
 export default ({ children, title }) => (
-  <Head>
-    <title>{title}</title>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link href="/static/style.css" rel="stylesheet" />
-    <link href="/static/logo.png" rel="icon" type="image/x-icon" />
-  </Head>
-  <main>
+  <section>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link href="/static/style.css" rel="stylesheet" />
+      <link href="/static/star.png" rel="icon" type="image/x-icon" />
+    </Head>
     {children}
-  </main>
+  </section>
 )
