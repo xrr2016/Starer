2651c3f213cd4e05628711c97c6568759b3de494
xrr2016 <xiaoranran1993@163.com>
2018-03-23 23:32:37 +0800

    :heavy_minus_sign:  test success

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="app">Hello from {{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      text: 'App'
+    }
+  }
+}
+</script>
+
+
+
+
